5c0073d70a6a95508074547e208e7a8cc4f8c13e
a5 <a5@LABSIS-A5M18>
2022-08-10 22:10:17 -0300

    Login version incompleta

--- src/BugTracker/BugTracker/frmLogin.cs
+++ src/BugTracker/BugTracker/frmLogin.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace BugTracker
 {
@@ -53,6 +54,32 @@ namespace BugTracker
 
         public bool ValidarCredenciales(string pUsuario, string pPassword)
         {
+
+            //Inicializamos la variable usuarioValido en false, para que solo si el usuario es valido retorne true
+            bool UsuarioValido = false;
+
+            //Creamos una coneccion a una Base de datos nueva
+            SqlConnection conexion = new SqlConnection();
+
+            //Definimos la cadena de conexion a la BD
+            conexion.ConnectionString = "Data Source=maquis;Initial Catalog=BugTracker;User ID=avisuales1;Password=Pav1#2020Maquis";
+
+            //La setencia try ... catch nos permite "atrapar" exepciones (Errores) y mostrar un mensaje mas amigable 
+
+            try
+            {
+
+                //Abrimos la coneccion a la base de datos
+                conexion.Open();
+
+                //Construimos la consulta sql para buscar el usuario en la BD
+                string consultaSql = string.Concat("Select * From Usuarios Where usuario= '", pUsuario, "'");
+
+                //Creamos un objeto command para poder realizar dicha consulta a la BD
+                SqlCommand command = 
+
+            }
+
             return true;
         }
 
