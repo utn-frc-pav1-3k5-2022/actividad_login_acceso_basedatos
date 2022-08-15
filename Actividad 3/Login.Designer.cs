d0e0d24faece8ed9e0a8234793054e2bfb73ca3a
Juan Pablo Galindez <juampygalindez@gmail.com>
2022-08-15 15:10:40 -0300

    Error Exception

---
+++ Actividad 3/Login.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Actividad_3
+{
+    partial class Login
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUser = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Location = new System.Drawing.Point(63, 37);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(50, 15);
+            this.lblUser.TabIndex = 0;
+            this.lblUser.Text = "Usuario:";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(43, 87);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(70, 15);
+            this.lblPassword.TabIndex = 1;
+            this.lblPassword.Text = "Contraseña:";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(240, 157);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Ingresar";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(330, 157);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Salir";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(119, 34);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(222, 23);
+            this.txtUser.TabIndex = 4;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(119, 84);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(222, 23);
+            this.txtPassword.TabIndex = 5;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 203);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUser);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lblUser;
+        private Label lblPassword;
+        private Button btnLogin;
+        private Button btnExit;
+        private TextBox txtUser;
+        private TextBox txtPassword;
+    }
+}
---
+++ Actividad 3/Login.cs
@@ -0,0 +1,234 @@
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Actividad_3;
+
+public class DataManager
+{
+
+
+    private static DataManager instance;
+    private string string_conexion;
+
+    public DataManager()
+    {
+        var string_conexion = "Data Source=.\\SQLEXPRESS;Initial Catalog=BugTracker;Integrated Security=true;";
+
+    }
+
+    public static DataManager GetInstance()
+    {
+        if (instance == null)
+            instance = new DataManager();
+
+        return instance;
+    }
+
+    /// Resumen:
+    ///      Se utiliza para sentencias SQL del tipo “Select” con parámetros recibidos desde la interfaz
+    ///      La función recibe por valor una sentencia sql como string y un diccionario de objetos como parámetros
+    /// Devuelve:
+    ///      un objeto de tipo DataTable con el resultado de la consulta
+    /// Excepciones:
+    ///      System.Data.SqlClient.SqlException:
+    ///          El error de conexión se produce:
+    ///              a) durante la apertura de la conexión
+    ///              b) durante la ejecución del comando.
+    public DataTable ConsultaSQL(string strSql, Dictionary<string, object>? prs = null)
+    {
+
+        SqlConnection dbConnection = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        DataTable tabla = new DataTable();
+        try
+        {
+
+            dbConnection.ConnectionString = string_conexion;
+            dbConnection.Open();
+            cmd.Connection = dbConnection;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = strSql;
+
+            //Agregamos a la colección de parámetros del comando los filtros recibidos
+            if (prs != null)
+            {
+                foreach (var item in prs)
+                {
+                    cmd.Parameters.AddWithValue(item.Key, item.Value);
+                }
+            }
+
+            tabla.Load(cmd.ExecuteReader());
+            return tabla;
+        }
+        catch (Exception ex)
+        {
+            throw (ex);
+        }
+    }
+
+    /// Resumen:
+    ///     Se utiliza para sentencias SQL del tipo “Insert/Update/Delete”. Recibe por valor una sentencia sql como string
+    /// Devuelve:
+    ///      un valor entero con el número de filas afectadas por la sentencia ejecutada
+    /// Excepciones:
+    ///      System.Data.SqlClient.SqlException:
+    ///          El error de conexión se produce:
+    ///              a) durante la apertura de la conexión
+    ///              b) durante la ejecución del comando.
+    public int EjecutarSQL(string strSql, Dictionary<string, object>? prs = null)
+    {
+        // Se utiliza para sentencias SQL del tipo “Insert/Update/Delete”
+        SqlConnection dbConnection = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+
+        int rtdo = 0;
+
+        // Try Catch Finally
+        // Trata de ejecutar el código contenido dentro del bloque Try - Catch
+        // Si hay error lo capta a través de una excepción
+        // Si no hubo error
+        try
+        {
+            dbConnection.ConnectionString = string_conexion;
+            dbConnection.Open();
+            cmd.Connection = dbConnection;
+            cmd.CommandType = CommandType.Text;
+            // Establece la instrucción a ejecutar
+            cmd.CommandText = strSql;
+
+            //Agregamos a la colección de parámetros del comando los filtros recibidos
+            if (prs != null)
+            {
+                foreach (var item in prs)
+                {
+                    cmd.Parameters.AddWithValue(item.Key, item.Value);
+                }
+            }
+
+            // Retorna el resultado de ejecutar el comando
+            rtdo = cmd.ExecuteNonQuery();
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+        return rtdo;
+    }
+
+
+    /// Resumen:
+    ///     Se utiliza para sentencias SQL del tipo “Select”. Recibe por valor una sentencia sql como string
+    /// Devuelve:
+    ///      un valor entero
+    /// Excepciones:
+    ///      System.Data.SqlClient.SqlException:
+    ///          El error de conexión se produce:
+    ///              a) durante la apertura de la conexión
+    ///              b) durante la ejecución del comando.
+    public object ConsultaSQLScalar(string strSql)
+    {
+        SqlConnection dbConnection = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        try
+        {
+            dbConnection.ConnectionString = string_conexion;
+            dbConnection.Open();
+            cmd.Connection = dbConnection;
+            cmd.CommandType = CommandType.Text;
+            // Establece la instrucción a ejecutar
+            cmd.CommandText = strSql;
+            return cmd.ExecuteScalar();
+        }
+        catch (SqlException ex)
+        {
+            throw (ex);
+        }
+    }
+
+}
+
+public partial class Login : Form
+{
+    public Login()
+    {
+        InitializeComponent();
+    }
+
+    private void btnLogin_Click(object sender, EventArgs e)
+    {
+        //validamos que se haya ingresado un Usuario
+        if (txtUser.Text == "")
+        {
+            MessageBox.Show("Se debe ingresar un usuario.");
+            return;
+        }
+
+        //validamos que se haya ingresado una contraseña
+        if (txtPassword.Text == "")
+        {
+            MessageBox.Show("Se debe ingresar una contraseña.");
+            return;
+        }
+
+        if (ValidarCredenciales(txtUser.Text, txtPassword.Text))
+        {
+            MessageBox.Show("Usted a ingresado al sistema.");
+            this.Close();
+        }
+        else 
+        {
+            txtUser.Text = "";
+            txtPassword.Text = "";
+            MessageBox.Show("Usuario y/o contraseña incorrextos.");
+        }
+    }
+
+    public bool ValidarCredenciales(string txtUser, string txtPassword)
+    {
+        //Inicializamos la variable usuarioValido en false, para que solo si el usuario es valido retorne true
+        bool usuarioValido = false;
+
+        //La doble barra o */ nos permite escribir comentarios sobre nuestro codigo sin afectar su funcionamiento.
+
+        //La sentencia try...catch nos permite "atrapar" excepciones (Errores) y dar al usuario un mensaje más amigable.
+        try
+        {
+
+            //Construimos la consulta sql para buscar el usuario en la base de datos.
+            String consultaSql = string.Concat(" SELECT * ",
+                                               "   FROM Usuarios ",
+                                               "  WHERE usuario =  '", txtUser, "'");
+
+            //Usando el método GetDataManager obtenemos la instancia unica de DataManager (Patrón Singleton) y ejecutamos el método ConsultaSQL()
+            DataTable resultado = DataManager.GetInstance().ConsultaSQL(consultaSql);
+
+            //Validamos que el resultado tenga al menos una fila.
+            if (resultado.Rows.Count >= 1)
+            {
+                //En caso de que exista el usuario, validamos que password corresponda al usuario
+                if (resultado.Rows[0]["password"].ToString() == txtPassword)
+                {
+                    usuarioValido = true;
+                }
+            }
+
+        }
+        catch (SqlException ex)
+        {
+            //Mostramos un mensaje de error indicando que hubo un error en la base de datos.
+            MessageBox.Show(string.Concat("Error de base de datos: ", ex.Message), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        // Retornamos el valor de usuarioValido. 
+        return usuarioValido;
+    }
+
+
+
+
+    private void btnExit_Click(object sender, EventArgs e)
+    {
+        this.Close();
+    }
+}
