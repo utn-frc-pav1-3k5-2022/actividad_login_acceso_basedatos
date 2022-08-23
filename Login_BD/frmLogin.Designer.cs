f162ef5b793ac9966d7664c7b39d3086f41594ee
Matias Marino <boca_mati_07@hotmail.com>
2022-08-23 12:40:57 -0300

    haciendo ejercicio con BD

---
+++ Login_BD/frmLogin.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Login_BD
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_Usuario = new System.Windows.Forms.TextBox();
+            this.txt_Password = new System.Windows.Forms.TextBox();
+            this.lbln_Usuario = new System.Windows.Forms.Label();
+            this.lbln_Password = new System.Windows.Forms.Label();
+            this.btn_Ingresar = new System.Windows.Forms.Button();
+            this.btn_Salir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_Usuario
+            // 
+            this.txt_Usuario.Location = new System.Drawing.Point(154, 50);
+            this.txt_Usuario.Name = "txt_Usuario";
+            this.txt_Usuario.Size = new System.Drawing.Size(326, 27);
+            this.txt_Usuario.TabIndex = 0;
+            // 
+            // txt_Password
+            // 
+            this.txt_Password.Location = new System.Drawing.Point(154, 116);
+            this.txt_Password.Name = "txt_Password";
+            this.txt_Password.Size = new System.Drawing.Size(326, 27);
+            this.txt_Password.TabIndex = 1;
+            // 
+            // lbln_Usuario
+            // 
+            this.lbln_Usuario.AutoSize = true;
+            this.lbln_Usuario.Location = new System.Drawing.Point(70, 50);
+            this.lbln_Usuario.Name = "lbln_Usuario";
+            this.lbln_Usuario.Size = new System.Drawing.Size(59, 20);
+            this.lbln_Usuario.TabIndex = 2;
+            this.lbln_Usuario.Text = "Usuario";
+            // 
+            // lbln_Password
+            // 
+            this.lbln_Password.AutoSize = true;
+            this.lbln_Password.Location = new System.Drawing.Point(70, 116);
+            this.lbln_Password.Name = "lbln_Password";
+            this.lbln_Password.Size = new System.Drawing.Size(70, 20);
+            this.lbln_Password.TabIndex = 3;
+            this.lbln_Password.Text = "Password";
+            // 
+            // btn_Ingresar
+            // 
+            this.btn_Ingresar.Location = new System.Drawing.Point(301, 183);
+            this.btn_Ingresar.Name = "btn_Ingresar";
+            this.btn_Ingresar.Size = new System.Drawing.Size(94, 29);
+            this.btn_Ingresar.TabIndex = 4;
+            this.btn_Ingresar.Text = "Ingresar";
+            this.btn_Ingresar.UseVisualStyleBackColor = true;
+            this.btn_Ingresar.Click += new System.EventHandler(this.btn_Ingresar_Click);
+            // 
+            // btn_Salir
+            // 
+            this.btn_Salir.Location = new System.Drawing.Point(417, 183);
+            this.btn_Salir.Name = "btn_Salir";
+            this.btn_Salir.Size = new System.Drawing.Size(94, 29);
+            this.btn_Salir.TabIndex = 5;
+            this.btn_Salir.Text = "Salir";
+            this.btn_Salir.UseVisualStyleBackColor = true;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 224);
+            this.Controls.Add(this.btn_Salir);
+            this.Controls.Add(this.btn_Ingresar);
+            this.Controls.Add(this.lbln_Password);
+            this.Controls.Add(this.lbln_Usuario);
+            this.Controls.Add(this.txt_Password);
+            this.Controls.Add(this.txt_Usuario);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmLogin";
+            this.Text = "frmLogin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txt_Usuario;
+        private TextBox txt_Password;
+        private Label lbln_Usuario;
+        private Label lbln_Password;
+        private Button btn_Ingresar;
+        private Button btn_Salir;
+    }
+}
---
+++ Login_BD/frmLogin.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Login_BD
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Ingresar_Click(object sender, EventArgs e)
+        {
+            //validamos que haya ingresado un usuario
+            if (txt_Usuario.Text == "")
+            {
+                MessageBox.Show("Se debe ingresar un usuario.");
+                return;
+            }
+            // validamos que haya ingresado una contrasena
+            if (txt_Password.Text == "")
+            {
+                MessageBox.Show("Se debe ingresar una contrasena");
+                return ;
+            }
+
+            if (ValidarCredenciales(txt_Usuario.Text,txt_Password.Text))
+            {
+                MessageBox.Show("Usted ha ingresado al sistema");
+                this.Close();
+            }
+            else 
+            {
+                MessageBox.Show("Debe ingresar usuario y password valido");
+            }
+        }
+
+        //public bool ValidarCredenciales(String pUsuario, String pPassword)
+        //{
+        //    bool usuarioValido = false;
+        //    SqlConnection conexion = new SqlConnection();
+        //    conexion.ConnectionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=BugTracker93584;Integrated Security=True";
+
+        //    try
+        //    {
+        //        //Abrimos la conexion a la base de datos.
+        //        conexion.Open();
+
+        //        //Construimos la consulta sql para buscar el usuario en la base de datos.
+        //        String consultaSql = string.Concat(" SELECT * ",
+        //                                           "   FROM Usuarios ",
+        //                                           "  WHERE usuario =  '", pUsuario, "'");
+
+        //        //Creamos un objeto command para luego ejecutar la consulta sobre la base de datos
+        //        SqlCommand command = new SqlCommand(consultaSql, conexion);
+
+        //        // El metodo ExecuteReader retorna un objeto SqlDataReader con la respuesta de la base de datos. 
+        //        // Con SqlDataReader los datos se leen fila por fila, cambiando de fila cada vez que se ejecuta el metodo Read()
+        //        SqlDataReader reader = command.ExecuteReader();
+
+        //        // El metodo Read() lee la primera fila disponible, si NO existe una fila retorna false (la consulta no devolvio resultados).
+        //        if (reader.Read())
+        //        {
+        //            //En caso de que exista el usuario, validamos que password corresponda al usuario
+        //            if (reader["password"].ToString() == pPassword)
+        //            {
+        //                usuarioValido = true;
+        //            }
+        //        }
+
+        //    }
+        //    catch (SqlException ex)
+        //    {
+        //        //Mostramos un mensaje de error indicando que hubo un error en la base de datos.
+        //        MessageBox.Show(string.Concat("Error de base de datos: ", ex.Message), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        //    }
+        //    finally
+        //    {
+        //        //Preguntamos si el estado de la conexion es abierto antes de cerrar la conexion.
+        //        if (conexion.State == ConnectionState.Open)
+        //        {
+        //            //Cerramos la conexion
+        //            conexion.Close();
+        //        }
+        //    }
+
+        //    // Retornamos el valor de usuarioValido. 
+        //    return usuarioValido;
+        //}
+
+        public bool ValidarCredenciales(string pUsuario, string pPassword)
+        {
+            //Inicializamos la variable usuarioValido en false, para que solo si el usuario es valido retorne true
+            bool usuarioValido = false;
+
+            //La doble barra o */ nos permite escribir comentarios sobre nuestro codigo sin afectar su funcionamiento.
+
+            //La sentencia try...catch nos permite "atrapar" excepciones (Errores) y dar al usuario un mensaje más amigable.
+            try
+            {
+
+                //Construimos la consulta sql para buscar el usuario en la base de datos.
+                String consultaSql = string.Concat(" SELECT * ",
+                                                   "   FROM Usuarios ",
+                                                   "  WHERE usuario =  '", pUsuario, "'");
+
+                //Usando el método GetDataManager obtenemos la instancia unica de DataManager (Patrón Singleton) y ejecutamos el método ConsultaSQL()
+                DataTable resultado = DataManager.GetInstance().ConsultaSQL(consultaSql);
+
+                //Validamos que el resultado tenga al menos una fila.
+                if (resultado.Rows.Count >= 1)
+                {
+                    //En caso de que exista el usuario, validamos que password corresponda al usuario
+                    if (resultado.Rows[0]["password"].ToString() == pPassword)
+                    {
+                        usuarioValido = true;
+                    }
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                //Mostramos un mensaje de error indicando que hubo un error en la base de datos.
+                MessageBox.Show(string.Concat("Error de base de datos: ", ex.Message), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            // Retornamos el valor de usuarioValido. 
+            return usuarioValido;
+        }
+    }
+}
